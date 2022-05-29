da942daf4fe1905bdeaaee0e020fe1cb583cbf2c
induwara2005 <106483025+induwara2005@users.noreply.github.com>
2022-05-29 22:25:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['© 𝘐.𝘕.𝘋.𝘜.𝘞.𝘈.𝘙.𝘈.']
+global.premium = ['+94717303100']
+global.ownernomer = '+94717303100'
 global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.botname = '𝘉𝘖𝘛 ® 𝘿.𝙄.𝙇.𝘼.'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
